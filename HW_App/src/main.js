5b765bac9edf3812a279fb1fb58281817028e037
Felix <fprocacci@hotmail.com>
2020-12-19 10:41:06 -0500

    rollup config working

--- HW_App/src/main.js
+++ HW_App/src/main.js
@@ -1,12 +1,13 @@
 import App from './App.svelte';
+import 'blueprint-css/dist/blueprint.min.css';
 
-/*
+
 const app = new App({
-		target: document.body,
-		props: {
-				name: 'Stuart'
-		}
+    target: document.body,
+    props: {
+        name: 'Stuart'
+    }
 });
-*/
+
 
 export default app;
